80673cb61c205d47030d5aced76b1ea45c7c43e2
jjminezs19 <jjimenez@mybookie.cr>
2017-01-27 11:21:30 -0500

    add contact

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+	var url = window.location.href;
+	
+	if(url == "http://localhost:9000/index.html"){
+		$('.inicio-tab').addClass('active');
+	}else{
+		$('.contacto-tab').addClass('active');
+	}
+});
